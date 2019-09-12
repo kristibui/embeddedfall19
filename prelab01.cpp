6fdff043496915c488a65686b2f3dbe535dbb2fe
Kristi Bui <bui.k@husky.neu.edu>
2019-09-12 11:34:21 -0400

    add code for prelab 1

---
+++ prelab01.cpp
@@ -0,0 +1,57 @@
+
+// Example program
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+double *v;
+//pointer to the first element in a sequence of elements appearing consecutively in memory
+int count;
+//number of elements inserted in the array so far by the user.
+int size;
+// space currently allocated for the array, given in number of elements
+
+void Shrink() {
+
+}
+
+
+void InsertElement() {
+
+}
+
+
+void RemoveElement() {
+
+}
+
+
+void AddElement() {
+
+}
+
+
+void PrintVector() {
+
+}
+
+
+void Grow() {
+
+}
+
+
+void Initialize() {
+        count = 0;
+        size = 2;
+        v = new double[size];
+        v[0] = 0;
+        v[1] = 1;
+}
+
+
+^G Get Help       ^O WriteOut       ^R Read File      ^Y Prev Page      ^K Cut Text       ^C Cur Pos
+^X Exit           ^J Justify        ^W Where Is       ^V Next Page      ^U UnCut Text     ^T To Spell

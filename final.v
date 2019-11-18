192cb1ba2405ab7f1cdbb894396e054dd3aa2108
Angela Wang <angelawang2416@gmail.com>
2019-11-18 12:50:11 -0500

    final

--- final.v
+++ final.v
@@ -20,24 +20,30 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module final(button, clk, S, LEDs, signalout);
-input button;
+module final(buttons, clk, S, LEDs, signalout, buttonsout);
+input [3:0]buttons;
 input clk;
 output [6:0] S; //for 7-segment display
 output [7:0] LEDs;
 output signalout;
+output [3:0] buttonsout;
 reg [6:0] S;
 reg [7:0] LEDs;
 reg signalout;
+reg [3:0] buttonsout;
 reg [2:0] progress;
 reg [31:0] counter;
-reg val1, val2, val;
-
+reg val1, val2, val3, val4;
+reg val1a, val2a, val3a, val4a;
+reg val1b, val2b, val3b, val4b;
+reg buttonpressed;
 initial begin
     LEDs = 8'b0000_0000;
     S = 7'b000_0000;
     signalout = 0;
     progress = 3'b000;
+    buttonsout = 4'b0000;
+    buttonpressed = 8'b0000_0000;
 end
 
     always @(posedge clk) begin
@@ -50,7 +56,9 @@ end
                 counter = counter + 1'b1;
                 if (counter >= 10) begin
                     progress = 3'b001;
-                    val1 = button;
+                    buttonsout = 4'b0111;
+                    buttonpressed = 8'b1000_0000;
+                    val1a = buttons;
                     counter = 0;
                 end
             end
@@ -58,12 +66,13 @@ end
                 counter = counter + 1'b1;
                 if (counter >= 1000000) begin //wait 10 ms before reading the switch again
                     progress = 3'b010;
-                    val2 = button;
+                    buttonsout = 4'b0111;
+                    val1b = buttons;
                     counter = 0;
                 end
             end
             3'b010: begin
-                if (val1 == val2 && val != val1) begin
+                if (val1a == val1b && val1 != val1a) begin
                     progress = 3'b011;
                 end
                 else begin
@@ -71,21 +80,40 @@ end
                 end
             end
             3'b011: begin
-                if (val1 == 1) begin
-                    LEDs = LEDs + 8'b0000_0001;
-                    if (LEDs == 8'b0000_0001)
-                        S = 7'b111_1100;
-                    else if (LEDs == 8'b0000_0010)
-                        S = 7'b011_1110;
-                    else if (LEDs == 8'b0000_0011) begin
-                        S = 7'b000_0110;
-                        LEDs = 8'b0000_0000;
-                    end
-                    val = 1;
-                end
-                else begin
-                    val = 0;
-                end
+                buttonpressed = buttonpressed + val1b;
+                LEDs = buttonpressed;
+//                case(buttons)     
+//                    4'b0111: begin
+//                        LEDs = 8'b0000_0001;
+//                    end
+                    
+//                    4'b1011: begin
+//                        LEDs = 8'b0000_0010;
+//                    end
+                    
+//                    4'b1101: begin
+//                        LEDs = 8'b0000_0100;
+//                    end
+                    
+//                    4'b1110: begin
+//                        LEDs = 8'b0000_1000;
+//                    end
+//                endcase
+//                if (val1 == 1) begin
+//                    LEDs = LEDs + 8'b0000_0001;
+//                    if (LEDs == 8'b0000_0001)
+//                        S = 7'b111_1100;
+//                    else if (LEDs == 8'b0000_0010)
+//                        S = 7'b011_1110;
+//                    else if (LEDs == 8'b0000_0011) begin
+//                        S = 7'b000_0110;
+//                        LEDs = 8'b0000_0000;
+//                    end
+//                    val = 1;
+//                end
+//                else begin
+//                    val = 0;
+//                end
                 counter = 0;
                 progress = 3'b000;
 
